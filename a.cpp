a34331a546d0ff3950bff41d4518595edec5889a
Yash Chauhan <yc20102002@gmail.com>
2020-10-01 18:41:03 +0530

    avg of two no.

---
+++ a.cpp
@@ -0,0 +1,46 @@
+																//  SMOKE SHISHA PLAY FIFA  // 
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define pll pair<ll,ll>
+#define MP make_pair
+#define ff first
+#define ss second
+#define PB push_back
+#define mod 1000000007
+#define lp(i,start,end) for(ll i=start;i<=end;i++) 
+#define deb1(a) cout<<#a<<" = "<<(a)<<endl;
+#define deb2(a,b) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<endl;
+#define deb3(a,b,c) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<", "<<#c<<" = "<<(c)<<endl;
+
+
+ll modu(ll a, ll b)
+{
+	ll ans =1;
+	while(b>0)
+	{
+		if(b&1)
+		ans = (ans*a)%mod;
+		b/=2;
+		a = (a*a)%mod;
+	}
+	return ans;
+}
+
+int main()
+{
+	fastio
+	ll t;
+	cin >> t;
+	while(t--)
+	{ 
+		ll a,b;
+		cin>>a>>b;
+		cout<<(a+b)/(2.0)<<endl;t	
+	}
+	return 0;	
+}
